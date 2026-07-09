1996947f3e336f32612e5bc3827f6f614a4674f1
Gbariela Paola Perez Ornelas <perezornelasgabrielapaola@gmail.com>
2026-07-09 01:02:27 -0600

    agregue interfaces

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login'; 
+import { Roles } from './roles/roles'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  
+  { path: 'login', component: Login },
+  { path: 'roles', component: Roles } 
+];
