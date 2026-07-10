5b9bd19c3486d016872fda5b780f0b705b4b9a8e
Gbariela Paola Perez Ornelas <perezornelasgabrielapaola@gmail.com>
2026-07-09 23:38:12 -0600

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login'; 
 import { Roles } from './roles/roles'; 
+import { Inicio } from './inicio/inicio';
 import { AdminLayoutLayout } from './shared/admin-layout/admin-layout.layout';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'admin/roles', pathMatch: 'full' },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   
   { path: 'login', component: Login },
-
+  { path: 'inicio', component: Inicio }, 
   {
     path: 'admin',
     component: AdminLayoutLayout,
     children: [
       { path: 'roles', component: Roles },
-      { path: 'inicio', component: Roles },
       { path: 'carrito', component: Roles },
       { path: 'compras', component: Roles },
       { path: 'productos', component: Roles },
@@ -24,6 +24,5 @@ export const routes: Routes = [
       { path: 'ventas', component: Roles }
     ]
   },
-
-  { path: '**', redirectTo: 'admin/roles' }
+  { path: '**', redirectTo: 'inicio' }
 ];
