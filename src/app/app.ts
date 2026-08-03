394cc8460cb393357d6a6698ff46209e42230698
IDGS-904_23002209 <oropezajim11@gmail.com>
2026-08-03 16:18:20 -0600

    cambio

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+<<<<<<< Updated upstream
 export class App {}
+=======
+export class App {
+
+  private http = inject(HttpClient);
+
+  mensaje = signal('Cargando...');
+
+  ngOnInit() {
+    this.http.get<any>('https://fl6rbp6p-7046.usw3.devtunnels.ms/api/api')
+      .subscribe({
+        next: (data) => {
+          this.mensaje.set(data.mensaje);
+        },
+        error: (err) => {
+          console.error(err);
+          this.mensaje.set('Error al conectar con el backend');
+        }
+      });
+  }
+}
+>>>>>>> Stashed changes
