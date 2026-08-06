39c9945dd7c63a7de62f18fb4ba21c78a3aedc1a
IDGS-904_23002209 <oropezajim11@gmail.com>
2026-08-05 18:42:45 -0600

    quite comentarios

--- src/app/productos/producto.service.ts
+++ src/app/productos/producto.service.ts
@@ -1,4 +1,4 @@
-// src/app/services/producto.service.ts
+
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
